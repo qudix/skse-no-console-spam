60d8241b19f294bf4bbd8da6b3cdaf7e2dd4bb6d
Qudix <17361645+Qudix@users.noreply.github.com>
2024-05-11 20:27:18 -0500

    chore: naming

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ set_xmakever("2.8.2")
 includes("lib/commonlibsse-ng")
 
 -- set project
-set_project("no-console-spam")
+set_project("skse-no-console-spam")
 set_version("1.0.0")
 set_license("GPL-3.0")
 
@@ -25,13 +25,13 @@ set_policy("package.requires_lock", true)
 set_config("skyrim_vr", false)
 
 -- targets
-target("no-console-spam")
+target("skse-no-console-spam")
     -- add dependencies to target
     add_deps("commonlibsse-ng")
 
     -- add commonlibsse-ng plugin
     add_rules("commonlibsse-ng.plugin", {
-        name = "no-console-spam",
+        name = "skse-no-console-spam",
         author = "qudix",
         description = "BEGONE CONSOLE SPAM!"
     })
