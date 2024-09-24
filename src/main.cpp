ca941dff5ffded8f8483cd2dfbd3ee2b8ec21935
Qudix <17361645+Qudix@users.noreply.github.com>
2024-09-24 12:02:05 -0500

    feat: update

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,3 @@
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/msvc_sink.h>
-
-void InitLogging()
-{
-    auto path = logs::log_directory();
-    if (!path)
-        return;
-
-    const auto plugin = SKSE::PluginDeclaration::GetSingleton();
-    *path /= std::format("{}.log", plugin->GetName());
-
-    std::vector<spdlog::sink_ptr> sinks{ 
-        std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true), 
-        std::make_shared<spdlog::sinks::msvc_sink_mt>() 
-    };
-
-    auto logger = std::make_shared<spdlog::logger>("global", sinks.begin(), sinks.end());
-    logger->set_level(spdlog::level::info);
-    logger->flush_on(spdlog::level::info);
-
-    spdlog::set_default_logger(std::move(logger));
-    spdlog::set_pattern("[%^%L%$] %v");
-}
-
 struct ConsoleLogHook
 {
     static void Print(RE::ConsoleLog* a_log, const char* a_fmt, ...)
@@ -411,16 +386,10 @@ struct ConsoleLogHook
 
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-    InitLogging();
-
-    const auto plugin = SKSE::PluginDeclaration::GetSingleton();
-    logs::info("{} v{} is loading...", plugin->GetName(), plugin->GetVersion());
-
     SKSE::Init(a_skse);
     SKSE::AllocTrampoline(48);
-    ConsoleLogHook::Install();
 
-    logs::info("{} loaded.", plugin->GetName());
+    ConsoleLogHook::Install();
 
     return true;
 }
