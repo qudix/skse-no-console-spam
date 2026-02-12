3cb427836bb0901f298baf1848fdfbe404c95e4d
Qudix <17361645+Qudix@users.noreply.github.com>
2026-02-11 22:04:36 -0600

    feat: update lib

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@ struct Hook_TESConditionItem
         return _IsTrue1(a_this, a_params);
     }
 
-    static inline REL::Hook _IsTrue1{ "IsTrue1", REL::ID(24633), 0x076, IsTrue };
-    static inline REL::Hook _IsTrue2{ "IsTrue2", REL::ID(29895), 0x0DF, IsTrue };
-    static inline REL::Hook _IsTrue3{ "IsTrue3", REL::ID(29895), 0x16C, IsTrue };
-    static inline REL::Hook _IsTrue4{ "IsTrue4", REL::ID(29923), 0x01E, IsTrue };
+    static inline REL::THook _IsTrue1{ "IsTrue1", REL::ID(24633), 0x076, IsTrue };
+    static inline REL::THook _IsTrue2{ "IsTrue2", REL::ID(29895), 0x0DF, IsTrue };
+    static inline REL::THook _IsTrue3{ "IsTrue3", REL::ID(29895), 0x16C, IsTrue };
+    static inline REL::THook _IsTrue4{ "IsTrue4", REL::ID(29923), 0x01E, IsTrue };
 };
 
 SKSE_PLUGIN_LOAD(const SKSE::LoadInterface* a_skse)
